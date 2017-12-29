f6e456685bb5c9436700880dbeba69c6c3c08ad1
arpit15 <arpit15945@gmail.com>
2017-12-28 22:34:13 -0500

    var name correction

--- HER/envs/baxter_orient_left_cts_anglebw.py
+++ HER/envs/baxter_orient_left_cts_anglebw.py
@@ -112,7 +112,7 @@ class BaxterEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         target_x, target_y = self.data.site_xpos[2][:2]
 
         angle_bw_target_block = atan2(target_y - box_y, target_x - box_x)
-        angle_bw_block_gripper = atan2(box_y - gripper_y, box_x - gripper_x)
+        angle_bw_block_gripper = atan2(box_y - ee_y, box_x - ee_x)
         angle = ((angle_bw_target_block - angle_bw_block_gripper) - pi)
 
         state = np.array([ee_x, ee_y, box_x, box_y, angle, target_x, target_y])
