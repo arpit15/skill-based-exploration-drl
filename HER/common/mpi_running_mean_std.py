fd954d6d07189800a59ee685da6270538594805c
arpit15 <arpit15945@gmail.com>
2018-02-05 18:12:05 -0500

    stupid import mistake

--- HER/common/mpi_running_mean_std.py
+++ HER/common/mpi_running_mean_std.py
@@ -1,5 +1,5 @@
 from mpi4py import MPI
-import tensorflow as tf, baselines.common.tf_util as U, numpy as np
+import tensorflow as tf, HER.common.tf_util as U, numpy as np
 from HER import logger 
 
 class RunningMeanStd(object):
