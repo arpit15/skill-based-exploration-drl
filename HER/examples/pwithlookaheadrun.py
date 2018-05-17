f4ecec6b13d863f8c92a580ed6898d9800e35dc5
arpit15 <arpit15945@gmail.com>
2018-05-16 23:23:47 -0400

    git logger level corrected before dumping any info in rank 0

--- HER/examples/pwithlookaheadrun.py
+++ HER/examples/pwithlookaheadrun.py
@@ -1,4 +1,4 @@
-import argparse
+[import argparse
 import time
 import os
 import logging
@@ -190,6 +190,7 @@ if __name__ == '__main__':
     args = parse_args()
    
     if MPI.COMM_WORLD.Get_rank() == 0:
+        logger.set_level(logger.DEBUG)
         logger.configure(dir=args["log_dir"])
         logger.debug(str(args))
         
