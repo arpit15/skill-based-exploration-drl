:sparkles: added gumbel softmax and grad zeroing to pass correct action to critic

HER/examples/pwithlookaheadrun.py
@@ -56,6 +56,9 @@ def run(env_id, seed, noise_type, layer_norm, evaluation, **kwargs):
     tf.reset_default_graph()
     ## NOTE: do tf things after this line
 
+    if kwargs['select_action']:
+        assert kwargs['skillset'], 'Skillset should be given for selecting action'
+
     # importing the current skill configs
     if kwargs['skillset']:
         # import HER.skills.set2 as skillset_file
@@ -164,17 +167,18 @@ def parse_args():
     parser.add_argument('--save-freq', type=int, default=1)
     parser.add_argument('--restore-dir', type=str, default=None)
     boolean_flag(parser, 'dologging', default=True)
-    boolean_flag(parser, 'invert-grad', default=False)
     
     boolean_flag(parser, 'actor-reg', default=True)
     boolean_flag(parser, 'tf-sum-logging', default=False)
 
-    parser.add_argument('--skillset', type=str, default='set10')
+    parser.add_argument('--skillset', type=str, default='set13')
     parser.add_argument('--commit-for', type=int, default=1)
 
     boolean_flag(parser, 'look-ahead', default=True)
     parser.add_argument('--exploration-final-eps', type=float, default=0.001)
     parser.add_argument('--num-samples', type=int, default=5)
+
+    boolean_flag(parser, 'select-action', default=True)
 
     args = parser.parse_args()
     # we don't directly specify timesteps for this script, so make sure that if we do specify them

HER/pddpg/custom_op.py
@@ -14,7 +14,7 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
     :return:
     """
     # Generate Random Gradient name in order to avoid conflicts with inbuilt names
-    rnd_name = 'PyFuncGrad' + 'ABC@a1b2c3'
+    rnd_name = 'ParamSelectorGrad' + 'ABC@a1b2c3'
 
     # Register Tensorflow Gradient
     tf.RegisterGradient(rnd_name)(grad)
@@ -23,25 +23,21 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
     g = tf.get_default_graph()
 
     # Add gradient override map
-    with g.gradient_override_map({"PyFunc": rnd_name, "PyFuncStateless": rnd_name}):
+    with g.gradient_override_map({"ParamSelector": rnd_name, "ParamSelectorStateless": rnd_name}):
         return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
 
 
 
-def my_identity_func(action, range_min, range_max):
+def my_identity_func(action, select_vec):
 	return action
 
 
 # @tf.RegisterGradient("MyIdentity")
 def _custom_identity_grad(op, grad):
 	action = op.inputs[0]
-	range_min = op.inputs[1]
-	range_max = op.inputs[2]
-
-	return tf.where( grad>0,
-                tf.multiply(grad, (range_max-action)/(range_max - range_min)),
-                tf.multiply(grad, (action- range_min)/(range_max - range_min))
-                ), tf.constant(0.), tf.constant(0.)
+	select_vec = op.inputs[1]
+	return tf.multiply(grad, select_vec), tf.zeros_like(grad)
+                
 
 if __name__ == "__main__":
 	with tf.Session() as sess:

HER/pddpg/ddpg.py
@@ -13,6 +13,8 @@ from HER.common.mpi_running_mean_std import RunningMeanStd
 from HER.ddpg.util import reduce_std, mpi_mean
 
 from HER.ddpg import custom_op as my_op
+from HER.pddpg import custom_op as grad_manipulation_op
+
 
 def normalize(x, stats):
     if stats is None:
@@ -56,13 +58,25 @@ def get_perturbed_actor_updates(actor, perturbed_actor, param_noise_stddev):
     return tf.group(*updates)
 
 
+def choose_actions(action, skillset, W_select, return_mask = False):
+    discrete_action = action[:, :skillset.len]
+    cts_parameters = action[:, skillset.len:]
+    selection_mask = tf.matmul(discrete_action, W_select)
+    selected_cts_params = tf.multiply(selection_mask, cts_parameters)
+    choose_actions_tf = tf.concat(values=[discrete_action, selected_cts_params], axis=-1)
+    if return_mask:
+        return choose_actions_tf, selection_mask
+    else:
+        return choose_actions_tf
+
+
 class DDPG(object):
     def __init__(self, actor, critic, memory, observation_shape, action_shape, param_noise=None, action_noise=None,
         gamma=0.99, tau=0.001, normalize_returns=False, enable_popart=False, normalize_observations=True,
         batch_size=128, observation_range=(-5., 5.), action_range=(-1., 1.), return_range=(-np.inf, np.inf),
         adaptive_param_noise=True, adaptive_param_noise_policy_threshold=.1,
         critic_l2_reg=0., actor_lr=1e-4, critic_lr=1e-3, clip_norm=None, reward_scale=1.,
-        inverting_grad = False, actor_reg=True):
+        actor_reg=True, select_action=False, skillset=None):
         
         logger.debug("Parameterized DDPG params")
         logger.debug(str(locals()))
@@ -75,6 +89,9 @@ class DDPG(object):
         self.actions = tf.placeholder(tf.float32, shape=(None,) + action_shape, name='actions')
         self.critic_target = tf.placeholder(tf.float32, shape=(None, 1), name='critic_target')
         self.param_noise_stddev = tf.placeholder(tf.float32, shape=(), name='param_noise_stddev')
+
+        if select_action:
+            self.temperature = tf.placeholder(tf.float32, shape=(), name = 'temperature')
 
         # Parameters.
         self.gamma = gamma
@@ -97,7 +114,7 @@ class DDPG(object):
         self.batch_size = batch_size
         self.stats_sample = None
         self.critic_l2_reg = critic_l2_reg
-        self.inverting_grad = inverting_grad
+        self.select_action = select_action
         self.actor_reg = actor_reg
 
         # Observation normalization.
@@ -118,6 +135,19 @@ class DDPG(object):
         else:
             self.ret_rms = None
 
+        # action selection constant
+        if self.select_action:
+            self.skillset = skillset
+            W_select = np.zeros((skillset.len, skillset.num_params))
+            for i in range(skillset.len):
+                starting_idx = skillset.params_start_idx[i]
+                ending_idx = starting_idx + skillset.skillset[i].num_params
+                W_select[i, starting_idx:ending_idx] = 1.
+
+            print("Selection matrix:%r"%W_select)
+            self.W_select = tf.constant(W_select, dtype= tf.float32, name="selection_mat")
+            
+
         # Create target networks.
         target_actor = copy(actor)
         target_actor.name = 'target_actor'
@@ -129,17 +159,26 @@ class DDPG(object):
         # Create networks and core TF parts that are shared across setup parts.
         self.normalized_critic_tf = critic(normalized_obs0, self.actions)
         self.critic_tf = denormalize(tf.clip_by_value(self.normalized_critic_tf, self.return_range[0], self.return_range[1]), self.ret_rms)
-        Q_obs1 = denormalize(target_critic(normalized_obs1, target_actor(normalized_obs1)), self.ret_rms)
+        if self.select_action:
+            Q_obs1 = denormalize(target_critic(normalized_obs1, choose_actions(target_actor(normalized_obs1), skillset, self.W_select)), self.ret_rms)
+        else:
+            Q_obs1 = denormalize(target_critic(normalized_obs1, target_actor(normalized_obs1)), self.ret_rms)
+        
         self.target_Q = self.rewards + (1. - self.terminals1) * gamma * Q_obs1
 
 
         # clip the target Q value
         self.target_Q = tf.clip_by_value(self.target_Q, -1/(1- gamma), 0)
         
-        self.actor_tf = actor(normalized_obs0)
-        if inverting_grad:
-            actor_tf_clone_with_invert_grad = my_op.py_func(my_op.my_identity_func, [self.actor_tf, -1., 1.], self.actor_tf.dtype, name="MyIdentity", grad=my_op._custom_identity_grad)
-            self.actor_tf = tf.reshape(actor_tf_clone_with_invert_grad, tf.shape(self.actor_tf)) 
+        if self.select_action:
+            self.actor_with_all_params_tf = actor(obs=normalized_obs0, temperature = self.temperature)
+            # create np and then convert to tf.constant
+            actor_tf_with_chosen_action, selection_mask = choose_actions(self.actor_with_all_params_tf, skillset, self.W_select, True)
+            actor_tf_clone_with_chosen_action = grad_manipulation_op.py_func(grad_manipulation_op.my_identity_func, [actor_tf_with_chosen_action, selection_mask], self.actor_with_all_params_tf.dtype, name="MyIdentity", grad=grad_manipulation_op._custom_identity_grad)
+            self.actor_tf = tf.reshape(actor_tf_clone_with_chosen_action, tf.shape(self.actor_with_all_params_tf)) 
+        else:
+            self.actor_tf = actor(normalized_obs0)
+        
         self.normalized_critic_with_actor_tf = critic(normalized_obs0, self.actor_tf, reuse=True)
         self.critic_with_actor_tf = denormalize(tf.clip_by_value(self.normalized_critic_with_actor_tf, self.return_range[0], self.return_range[1]), self.ret_rms)
         
@@ -275,22 +314,41 @@ class DDPG(object):
         self.stats_ops = ops
         self.stats_names = names
 
-    def pi(self, obs, apply_noise=True, compute_Q=True):
+    def pi(self, obs, apply_noise=True, compute_Q=True, temperature = 1.):
         if self.param_noise is not None and apply_noise:
             actor_tf = self.perturbed_actor_tf
+        elif self.action_noise is not None and apply_noise and self.select_action:
+            actor_tf = self.actor_with_all_params_tf
         else:
             actor_tf = self.actor_tf
         feed_dict = {self.obs0: [obs]}
+        if self.select_action:
+            feed_dict[self.temperature] = temperature
+
         if compute_Q:
             action, q = self.sess.run([actor_tf, self.critic_with_actor_tf], feed_dict=feed_dict)
         else:
             action = self.sess.run(actor_tf, feed_dict=feed_dict)
             q = None
         action = action.flatten()
+
         if self.action_noise is not None and apply_noise:
             action = self.action_noise(action)
 
+        if self.select_action:
+            # zero out the params of not taken actions
+            chosen_action = np.argmax(action[:self.skillset.len])
+
+            continuous_actions = np.zeros_like(action[self.skillset.len:])
+            starting_idx = self.skillset.params_start_idx[chosen_action]
+            ending_idx = starting_idx + self.skillset.skillset[chosen_action].num_params
+            continuous_actions[starting_idx:ending_idx] = action[self.skillset.len + starting_idx: 
+                                                                self.skillset.len + ending_idx]
+
+            action[self.skillset.len:] = continuous_actions.copy()
+
         action = np.clip(action, self.action_range[0], self.action_range[1])
+        
         return action, q
 
     def store_transition(self, obs0, action, reward, obs1, terminal1):
@@ -299,7 +357,7 @@ class DDPG(object):
         if self.normalize_observations:
             self.obs_rms.update(np.array([obs0]))
 
-    def train(self, summary_var):
+    def train(self, summary_var, temperature = 1.):
         # Get a batch.
         batch = self.memory.sample(batch_size=self.batch_size)
 
@@ -333,7 +391,7 @@ class DDPG(object):
 
         # Get all gradients and perform a synced update.
         ops = [summary_var, self.actor_grads, self.actor_loss, self.critic_grads, self.critic_loss, self.target_Q]
-        current_summary, actor_grads, actor_loss, critic_grads, critic_loss, _ = self.sess.run(ops, feed_dict={
+        feed_dict={
             self.obs0: batch['obs0'],
             self.actions: batch['actions'],
             self.critic_target: target_Q,
@@ -341,12 +399,15 @@ class DDPG(object):
             self.obs1: batch['obs1'],
             self.rewards: batch['rewards'],
             self.terminals1: batch['terminals1'].astype('float32'),
-        })
+        }
+
+        if self.select_action:
+            feed_dict.update({self.temperature: temperature})
+
+        current_summary, actor_grads, actor_loss, critic_grads, critic_loss, _ = self.sess.run(ops, feed_dict = feed_dict)
         self.actor_optimizer.update(actor_grads, stepsize=self.actor_lr)
         self.critic_optimizer.update(critic_grads, stepsize=self.critic_lr)
 
-        # from ipdb import set_trace
-        # set_trace()
         return critic_loss, actor_loss, current_summary
 
     def initialize(self, sess):
@@ -359,15 +420,22 @@ class DDPG(object):
     def update_target_net(self):
         self.sess.run(self.target_soft_updates)
 
-    def get_stats(self):
+    def get_stats(self, temperature = 1.0):
+
         if self.stats_sample is None:
             # Get a sample and keep that fixed for all further computations.
             # This allows us to estimate the change in value for the same set of inputs.
             self.stats_sample = self.memory.sample(batch_size=self.batch_size)
-        values = self.sess.run(self.stats_ops, feed_dict={
+
+        feed_dict={
             self.obs0: self.stats_sample['obs0'],
             self.actions: self.stats_sample['actions'],
-        })
+        }
+
+        if self.select_action:
+            feed_dict[self.temperature] = temperature
+
+        values = self.sess.run(self.stats_ops, feed_dict=feed_dict)
 
         names = self.stats_names[:]
         assert len(names) == len(values)

HER/pddpg/models.py
@@ -1,6 +1,34 @@
 import tensorflow as tf
 import tensorflow.contrib as tc
 
+def sample_gumbel(shape, eps=1e-20): 
+  """Sample from Gumbel(0, 1)"""
+  U = tf.random_uniform(shape,minval=0,maxval=1)
+  return -tf.log(-tf.log(U + eps) + eps)
+
+def gumbel_softmax_sample(logits, temperature): 
+  """ Draw a sample from the Gumbel-Softmax distribution"""
+  y = logits + sample_gumbel(tf.shape(logits))
+  return tf.nn.softmax( y / temperature)
+
+def gumbel_softmax(logits, temperature, hard=False):
+  """Sample from the Gumbel-Softmax distribution and optionally discretize.
+  Args:
+    logits: [batch_size, n_class] unnormalized log-probs
+    temperature: non-negative scalar
+    hard: if True, take argmax, but differentiate w.r.t. soft sample y
+  Returns:
+    [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+    If hard=True, then the returned sample will be one-hot, otherwise it will
+    be a probabilitiy distribution that sums to 1 across classes
+  """
+  y = gumbel_softmax_sample(logits, temperature)
+  if hard:
+    k = tf.shape(logits)[-1]
+    #y_hard = tf.cast(tf.one_hot(tf.argmax(y,1),k), y.dtype)
+    y_hard = tf.cast(tf.equal(y,tf.reduce_max(y,1,keep_dims=True)),y.dtype)
+    y = tf.stop_gradient(y_hard - y) + y
+  return y
 
 class Model(object):
     def __init__(self, name):
@@ -20,13 +48,14 @@ class Model(object):
 
 
 class Actor(Model):
-    def __init__(self, discrete_action_size, cts_action_size, name='actor', layer_norm=True):
+    def __init__(self, discrete_action_size, cts_action_size, name='actor', layer_norm=True, select_action = True):
         super(Actor, self).__init__(name=name)
         self.discrete_action_size = discrete_action_size
         self.cts_action_size = cts_action_size
         self.layer_norm = layer_norm
+        self.select_action = select_action
 
-    def __call__(self, obs, reuse=False, num_layers=3, hidden_units=64):
+    def __call__(self, obs, reuse=False, num_layers=3, hidden_units=64, temperature = 1.):
         with tf.variable_scope(self.name) as scope:
             if reuse:
                 scope.reuse_variables()
@@ -40,13 +69,19 @@ class Actor(Model):
                 x = tf.nn.relu(x)
              
             x_discrete = tf.layers.dense(x, self.discrete_action_size, kernel_initializer=tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3))
+            
+            if self.select_action:
+                x_discrete = gumbel_softmax(logits=x_discrete, temperature=temperature, hard= True)
+
             x_cts = tf.layers.dense(x, self.cts_action_size, kernel_initializer=tf.random_uniform_initializer(minval=-3e-3, maxval=3e-3))
             
+            # [-1,1]
+            x_cts = tf.nn.tanh(x_cts)
+
             x = tf.concat(values=[x_discrete, x_cts], axis=-1)
             ## for the sake of quick recalling
             y = tf.identity(x, name="preactivation")
             
-            x = tf.nn.tanh(y)
         return x
 
 

HER/pddpg/noise.py
@@ -79,10 +79,10 @@ class EpsilonNormalParameterizedActionNoise(ActionNoise):
             action = np.concatenate((discrete_actions_prob, continuous_actions))
             return action
         else:
-            continuous_actions = np.random.uniform(-1. , 1., size= action.size - self.discrete_actions_dim)
             discrete_actions_prob = np.zeros((self.discrete_actions_dim,))
             chosen_action = np.random.choice(self.discrete_actions_dim)
             discrete_actions_prob[chosen_action] = 1.0
+            continuous_actions = np.random.uniform(-1. , 1., size= action.size - self.discrete_actions_dim)
             action = np.concatenate((discrete_actions_prob, continuous_actions))
             return action
 

HER/pddpg/training_with_look_ahead.py
@@ -38,10 +38,11 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
     else:
         tf_sum_logging = False
         
-    if "invert_grad" in kwargs: 
-        invert_grad = kwargs["invert_grad"]
+    if "select_action" in kwargs: 
+        select_action = kwargs["select_action"]
+        # add temperature and anneal
     else:
-        invert_grad = False
+        select_action = False
 
     if "actor_reg" in kwargs:
         actor_reg = kwargs["actor_reg"]
@@ -63,6 +64,7 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
 
     if kwargs['skillset']:
         action_shape = (kwargs['my_skill_set'].len + kwargs['my_skill_set'].num_params,)
+        my_skill_set = kwargs['my_skill_set']
     else:
         action_shape = env.action_space.shape
 
@@ -71,7 +73,8 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
         batch_size=batch_size, action_noise=action_noise, param_noise=param_noise, critic_l2_reg=critic_l2_reg,
         actor_lr=actor_lr, critic_lr=critic_lr, enable_popart=popart, clip_norm=clip_norm,
         reward_scale=reward_scale,
-        inverting_grad = invert_grad,
+        select_action = select_action,
+        skillset = my_skill_set if kwargs['skillset'] else None,
         actor_reg = actor_reg
         )
 
@@ -101,6 +104,9 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
             for var in critic_trainable_vars:
                 tf.summary.histogram(var.name, var)
 
+            if my_skill_set:
+                tf.summary.histogram("discrete_action", agent.actor_tf[:, :my_skill_set.len])
+                
             tf.summary.histogram("actions_out", agent.actor_tf)
             tf.summary.histogram("critic_out", agent.critic_tf)
             tf.summary.histogram("target_Q", agent.target_Q)
@@ -117,7 +123,6 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
         ## restore
         if kwargs['skillset']:
             ## restore skills
-            my_skill_set = kwargs['my_skill_set']
             my_skill_set.restore_skillset(sess=sess)
         ## restore current controller
         if kwargs["restore_dir"] is not None:
@@ -287,7 +292,7 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
                             if kwargs['skillset']:
                                 sub_states[:] = []
 
-                # print(rank, "Training!")
+                print(rank, "Training!")
                 # Train.
                 for t_train in range(nb_train_steps):
                     # print(rank, t_train)
@@ -304,7 +309,7 @@ def train(env, nb_epochs, nb_epoch_cycles, render_eval, reward_scale, render, pa
                     if dologging and tf_sum_logging and rank==0:                   
                         writer_t.add_summary(current_summary, epoch*nb_epoch_cycles*nb_train_steps + cycle*nb_train_steps + t_train)
 
-            # print("Evaluating!")
+            print("Evaluating!")
             # Evaluate after training is done.
             if (eval_env is not None) and rank==0:
                 for _ in range(nb_eval_episodes):