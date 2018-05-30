:bugs: corrected the gradient and selection matrix

HER/pddpg/custom_op.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np 
 
 # Python Custom Gradient Function
 def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
@@ -14,7 +14,7 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
     :return:
     """
     # Generate Random Gradient name in order to avoid conflicts with inbuilt names
-    rnd_name = 'ParamSelectorGrad' + 'ABC@a1b2c3'
+    rnd_name = 'PyFuncGrad' + 'ABC@a1b2c3'
 
     # Register Tensorflow Gradient
     tf.RegisterGradient(rnd_name)(grad)
@@ -23,32 +23,50 @@ def py_func(func, inp, Tout, stateful=True, name=None, grad=None):
     g = tf.get_default_graph()
 
     # Add gradient override map
-    with g.gradient_override_map({"ParamSelector": rnd_name, "ParamSelectorStateless": rnd_name}):
+    with g.gradient_override_map({"PyFunc": rnd_name, "PyFuncStateless": rnd_name}):
         return tf.py_func(func, inp, Tout, stateful=stateful, name=name)
 
 
 
 def my_identity_func(action, select_vec):
-	return action
+    # python func
+    return np.multiply(action, select_vec)
 
 
-# @tf.RegisterGradient("MyIdentity")
 def _custom_identity_grad(op, grad):
-	action = op.inputs[0]
-	select_vec = op.inputs[1]
-	return tf.multiply(grad, select_vec), tf.zeros_like(grad)
+    action = op.inputs[0]
+    select_vec = op.inputs[1]
+    return [tf.multiply(grad, select_vec), tf.zeros_like(grad)]
                 
 
 if __name__ == "__main__":
-	with tf.Session() as sess:
-		c = tf.constant([[-2.0, 0.0], [0.5, 2.0]])
-		s1 = py_func(my_identity_func, [c, -1., 1.], c.dtype, name="MyIdentity", grad=_custom_identity_grad)
-		s2 = -tf.reduce_mean(s1*2)
-		init = tf.global_variables_initializer()
-		sess.run(init)
-		print(c.eval(), s2.eval())
+    with tf.Session() as sess:
+        # c = tf.constant([[-2.0, 0.0], [0.5, 2.0]])
+        # s = tf.constant([[1.0, 0.0], [0.0, 1.0]])
 
-		## expected result
-		# [[ 0.25  -0.25 ]
- 		# [-0.375 -0.75 ]]
-		print(tf.gradients(s2, c)[0].eval())
+        c = tf.constant([[-2.0, 0.5]])
+        s = tf.constant([[1.0, 0.0]])
+
+        s1 = py_func(my_identity_func, [c, s], c.dtype, name="MyIdentity", grad=_custom_identity_grad)
+        s2 = -tf.reduce_mean(s1)
+        
+        dels1 = tf.gradients(s2, s1)[0]
+        dels = tf.gradients(s2, s)[0]
+        delc = tf.gradients(s2, c)[0]
+
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        # expected output: [[-2.   0.5]] 1.0
+        print(c.eval(), s2.eval())
+
+        print(dels1, dels, delc)
+
+        # expected output : [[-0.5 -0.5]]
+        print(dels1.eval())
+        
+        # expected output : [[0., 0.]]
+        print(dels.eval())
+        
+        # expected output : [[-0.5 -0.]]
+        print(delc.eval())

HER/pddpg/ddpg.py
@@ -62,8 +62,9 @@ def choose_actions(action, skillset, W_select, return_mask = False):
     discrete_action = action[:, :skillset.len]
     cts_parameters = action[:, skillset.len:]
     selection_mask = tf.matmul(discrete_action, W_select)
-    selected_cts_params = tf.multiply(selection_mask, cts_parameters)
-    choose_actions_tf = tf.concat(values=[discrete_action, selected_cts_params], axis=-1)
+    # selected_cts_params = tf.multiply(selection_mask, cts_parameters)
+    # choose_actions_tf = tf.concat(values=[discrete_action, selected_cts_params], axis=-1)
+    choose_actions_tf = tf.multiply(selection_mask, action)
     if return_mask:
         return choose_actions_tf, selection_mask
     else:
@@ -138,7 +139,8 @@ class DDPG(object):
         # action selection constant
         if self.select_action:
             self.skillset = skillset
-            W_select = np.zeros((skillset.len, skillset.num_params))
+            W_select = np.zeros((skillset.len, skillset.num_params + skillset.len))
+            W_select[:skillset.len, :skillset.len] = 1.
             for i in range(skillset.len):
                 starting_idx = skillset.params_start_idx[i]
                 ending_idx = starting_idx + skillset.skillset[i].num_params