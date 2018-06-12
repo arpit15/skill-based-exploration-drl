db08079caca13e5328686b90bdc02c1021a22e45
arpit agarwal <arpit15945@gmail.com>
2018-06-12 18:51:29 -0400

    added regressor with larger network

---
+++ HER/successor_prediction_model/v2/models.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import os.path as osp
+
+from HER.ddpg.models import Model
+from HER.deepq.models import _mlp
+
+class regressor:
+    def __init__(self, name, in_shape, out_shape, sess=None, log_dir="/tmp"):
+        # properties
+        self.in_shape = in_shape
+        self.out_shape = out_shape
+
+        # tf
+        self.in_tensor = tf.placeholder(tf.float32, shape=(None,) + (in_shape,), name='state_goal')
+        self.out_tensor = _mlp(inpt=self.in_tensor, hiddens=[1000,1000,1000],scope="suc_pred_model", num_actions= out_shape,layer_norm=True)
+        self.target_tensor = tf.placeholder(tf.float32, shape=(None,) + (out_shape,), name='final_state')
+        
+        self.sess = sess
+        self.log_dir = log_dir
+        # loss function 
+        self.loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.target_tensor - self.out_tensor), axis=1))
+
+        self.sqrt_loss = tf.reduce_mean(tf.sqrt(tf.reduce_sum(tf.square(self.target_tensor - self.out_tensor), axis=1)))
+        # summary
+        tf.summary.histogram("input", self.in_tensor)
+        tf.summary.histogram("output", self.out_tensor)
+        tf.summary.histogram("outputVstarget", self.target_tensor - self.out_tensor)
+        tf.summary.scalar("loss", self.loss)
+        tf.summary.scalar("sqrt loss", self.sqrt_loss)
+        self.sum = tf.summary.merge_all()
+
+        # optim
+        self.lr = tf.placeholder(tf.float32,(),'learning_rate')
+        self.vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        self.optim = tf.train.AdamOptimizer(self.lr) \
+                          .minimize(self.loss, var_list=(self.vars))
+
+        # saver
+        self.saver = tf.train.Saver(var_list = self.vars, max_to_keep=5, 
+                                        name="%s_suc_pred_net"%name, 
+                                        keep_checkpoint_every_n_hours=1,
+                                        save_relative_paths=True)
+
+        self.writer_t = tf.summary.FileWriter(osp.join(self.log_dir ,'tfsum' ,'train'), sess.graph)
+        self.writer_v = tf.summary.FileWriter(osp.join(self.log_dir , 'tfsum', 'test'), sess.graph)
+
+
+    def train(self, num_epochs, batch_size, lr=1e-3, train_dataset = None, test_dataset=None, test_freq = 1, save_freq=10, log=True):
+        feed_dict = {self.lr :lr}
+        
+        for curr_epoch in range(num_epochs):
+
+            # train_feats, train_label = self.sess.run(train_dataset)
+            curr_train_data = train_dataset.sample(batch_size, axis=0).as_matrix()
+            
+            feed_dict[self.in_tensor] = curr_train_data[:, :self.in_shape]
+            feed_dict[self.target_tensor] = curr_train_data[:,self.in_shape:]
+
+            train_summary, train_loss, _ = self.sess.run([self.sum, self.loss, self.optim], feed_dict)
+            if log:
+                print("epoch:%d, loss:%.5f"%(curr_epoch+1, train_loss))
+
+                self.writer_t.add_summary(train_summary,curr_epoch)
+
+
+            # test
+            if (curr_epoch+1)%test_freq == 0:
+                test_summary = self.test(test_dataset)
+                if log:
+                    self.writer_v.add_summary(test_summary,curr_epoch)
+
+
+            if (curr_epoch+1)%save_freq == 0:
+                self.save()
+
+            # if (curr_epoch+1)%10:
+            #     print("epoch:%d, loss:%.5f"%(curr_epoch+1, train_loss))
+
+        print(num_epochs)
+
+    def save(self, path=None):
+        if path is None:
+            path = osp.join(self.log_dir, "suc_pred_model")
+        self.saver.save(self.sess, path)
+
+    def test(self, test_dataset):
+        test_feats, test_label = test_dataset
+        test_summary = self.sess.run(self.sum, feed_dict={
+                                        self.in_tensor: test_feats,
+                                        self.target_tensor: test_label
+                                        })
+        # test_loss = 0.
+        # i = 0
+        # while True:
+        #     try:
+        #         i += 1
+        #         test_feats, test_label = self.sess.run(test_dataset)
+
+        #         test_loss += self.sess.run(self.loss, feed_dict={
+        #                                 self.in_tensor: test_feats,
+        #                                 self.target_tensor: test_label
+        #                                 })
+
+        #         print(test_feats.shape, test_loss)
+
+
+        #     except tf.errors.OutOfRangeError:
+        #         break
+        
+        # print("test loss:%f, %.4f"%(test_loss, test_loss))
+        # test_summary = tf.Summary(value=[tf.Summary.Value(tag="loss",simple_value=test_loss/i)])
+        return test_summary
+
---
+++ HER/successor_prediction_model/v2/train.py
@@ -0,0 +1,109 @@
+import argparse
+import os
+import gym
+import csv
+import os.path as osp
+import tensorflow as tf
+import numpy as np 
+from tqdm import tqdm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from HER.common.misc_util import (
+    set_global_seeds,
+    boolean_flag,
+)
+
+from HER.successor_prediction_model.v0.models import regressor
+import HER.envs
+from HER.ddpg.skills import DDPGSkill
+import HER.common.tf_util as U
+
+def run(env_id, render, log_dir, 
+            train_epoch, batch_size=32, lr = 1e-3, seed = 0, whiten = False):
+    
+    env = gym.make(env_id)
+    observation_shape = env.observation_space.shape[-1]
+    global in_size, out_size
+    in_size = observation_shape
+    out_size = observation_shape-3
+
+    set_global_seeds(seed)
+    # env.seed(seed)
+
+    
+    with U.single_threaded_session() as sess:
+        
+        pred_model = regressor(in_shape = in_size, 
+                            out_shape = out_size,
+                            name = "suc_pred_model", sess=sess,
+                            log_dir=log_dir)
+        
+
+
+        init_op = tf.group(tf.global_variables_initializer(),
+                               tf.local_variables_initializer(), 
+                               )
+        sess.run(init_op)
+
+        ## creating dataset tensors
+        csv_filename = osp.join(log_dir, "%s.csv"%env_id)
+
+        ## 
+        base_dataset = np.loadtxt(csv_filename, delimiter=',')
+        train, test = train_test_split(base_dataset, test_size=0.2)
+        train_feat = train[:,:-1]
+        train_labels = train[:,-1]
+        # print(train.shape, test.shape)
+
+        # whiten
+        if whiten:
+            train_feat_mean = np.mean(train_feat, axis=0)
+            train_feat_std = np.std(train_feat, axis=0)
+
+            # save mean and var
+            statistics = np.concatenate((train_feat_mean, train_feat_std))
+            with open(osp.join(log_dir, "%s_stat.npy"%env_id), 'wb') as f:
+                np.save(f, statistics)
+
+            # create pd
+            train_feat_dataset = ( ( train_feat - train_feat_mean)/train_feat_std)
+            print(train_feat_dataset.shape, train_labels[:, np.newaxis].shape)
+            train_dataset = pd.DataFrame(np.concatenate((train_feat_dataset, train_labels[:, np.newaxis]),axis=1))
+            
+            test_feat_dataset = ( ( test[:, :-1] - train_feat_mean)/train_feat_std)
+            test_dataset = [test_feat_dataset, test[:,[-1]]]
+            ####
+
+            print(train_dataset.shape, test_dataset[0].shape)
+        else:
+            train_dataset = pd.DataFrame(np.concatenate((train_feat, train_labels[:, np.newaxis]),axis=1))
+            test_dataset = [test[:, :-1], test[:,[-1]]]
+
+        pred_model.train(train_epoch, batch_size, lr, train_dataset , test_dataset)
+        pred_model.save()
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--env-id', type=str, default='Reacher3d-v0')
+    boolean_flag(parser, 'render', default=False)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    
+    parser.add_argument('--log-dir', type=str, default='/tmp/her')
+    
+    parser.add_argument('--train-epoch', type=int, default=10)
+
+    parser.add_argument('--batch-size', type=int, default=64)
+    boolean_flag(parser, 'whiten', default=False)
+
+    
+    args = parser.parse_args()
+    dict_args = vars(args)
+    return dict_args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    run(**args)
+    
