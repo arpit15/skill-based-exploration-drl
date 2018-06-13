a48b10b8c55fd0bccd302b675b785637a9fc01b5
arpit15 <arpit15945@gmail.com>
2018-06-12 21:47:27 -0400

    :bugs: working delta data creation

--- HER/successor_prediction_model/v3/builddelta_data.py
+++ HER/successor_prediction_model/v3/builddelta_data.py
@@ -4,17 +4,21 @@ from sys import argv
 
 
 env_name = argv[1]
+shape = int(argv[2])
+
 csv_filename = '/tmp/her/%s.csv' % env_name
 
 data = np.loadtxt(csv_filename, delimiter=',')
-tree = spatial.KDTree(data[:,:6])
+tree = spatial.KDTree(data[:,:shape])
 
-results = tree.query(data[:, :6],k=2)[1][:,1]
-s0 = data[-3:][results]
+results = tree.query(data[:, :shape],k=2)[1][:,1]
+s0 = data[:,shape:][results]
 
-delta = data[-3:] - s0
+delta = data[:, shape:] - s0
 
-delta_dataset = np.concatenate((data[:,:6], s0, delta))
+print(data[:,:shape].shape, s0.shape, delta.shape)
 
-np.save('/tmp/her/%s-delta.csv'%env_name)
+delta_dataset = np.concatenate((data[:,:shape], s0, delta),axis=1)
+
+np.savetxt('/tmp/her/%s-delta.csv'%env_name, delta_dataset, delimiter=',')
 
