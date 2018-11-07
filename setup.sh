109ab8fe85bc88a21fb5d08db6cfebf336514d8b
arpit agarwal <arpit15945@gmail.com>
2018-11-07 08:50:57 -0500

    :sparkles: added pip package installation

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get update -q \
     vim \
     virtualenv \
     wget 
-curl -o /usr/local/bin/patchelf https://s3-us-west-2.amazonaws.com/openai-sci-artifacts/manual-builds/patchelf_0.9_amd64.elf \
+sudo curl -o /usr/local/bin/patchelf https://s3-us-west-2.amazonaws.com/openai-sci-artifacts/manual-builds/patchelf_0.9_amd64.elf \
     && chmod +x /usr/local/bin/patchelf
 mkdir -p /root/.mujoco \
     && wget https://www.roboti.us/download/mjpro150_linux.zip -O mujoco.zip \
@@ -17,3 +17,13 @@ mkdir -p /root/.mujoco \
     && rm mujoco.zip
 
 source ~/mjc15/bin/activate
+
+cd ~/new_RL3
+wget https://github.com/openai/mujoco-py/archive/1.50.1.0.zip 
+unzip 1.50.1.0.zip
+cd ~/new_RL3/mujoco-py
+pip install -r requirements.txt
+pip install -r requirements.dev.txt
+pip install -e .
+
+
