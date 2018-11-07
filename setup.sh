a12131a913353e4c5397f61a59d6ef522d0d6501
arpit agarwal <arpit15945@gmail.com>
2018-11-07 08:12:33 -0500

    :sparkles: script for mujoco-py

--- setup.sh
+++ setup.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-sudo apt-get install libnlopt-dev git python-catkin-pkg vim ros-kinetic-desktop-full swig ros-kinetic-kdl-parser
+sudo apt-get update -q \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    curl \
+    git \
+    software-properties-common \
+    net-tools \
+    unzip \
+    vim \
+    virtualenv \
+    wget 
+curl -o /usr/local/bin/patchelf https://s3-us-west-2.amazonaws.com/openai-sci-artifacts/manual-builds/patchelf_0.9_amd64.elf \
+    && chmod +x /usr/local/bin/patchelf
+mkdir -p /root/.mujoco \
+    && wget https://www.roboti.us/download/mjpro150_linux.zip -O mujoco.zip \
+    && unzip mujoco.zip -d /root/.mujoco \
+    && rm mujoco.zip
 
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-
-pip install virtualenv ipdb
-
-export ENVHOME=${HOME}/new_RL3
-virtualenv -p python3.5 $ENVHOME
-
-source ${ENVHOME}/bin/activate
-pip install catkin_pkg
-mkdir -p ${HOME}/my_ws/src
-cd ${HOME}/my_ws/src
-
-git clone https://arpit_agarwal_@bitbucket.org/arpit_agarwal_/trac_ik.git
-cd ${HOME}/my_ws/src/trac_ik
-git checkout python_wrapper_separated_pkg
-cd ${HOME}/my_ws
-catkin_make -DPYTHON_EXECUTABLE=${ENVHOME}/bin/python
-ln -s ${HOME}/my_ws/devel/lib/python3/dist-packages/trac_ik_python ${ENVHOME}/lib/python3.5/site-packages
-
-cd ${ENVHOME}
-pip install mpi4py tensorflow-gpu==1.3.0 gym mujoco_py==0.5.7
-git clone https://arpit_agarwal_@bitbucket.org/arpit_agarwal_/hindsight_experience_replay.git
-cd ${ENVHOME}/hindsight_experience_replay
-ln -s ${ENVHOME}/hindsight_experience_replay/HER ${ENVHOME}/lib/python3.5/site-packages
+source ~/mjc15/bin/activate
